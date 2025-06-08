86017ae6d6bc508fdc443030166ee659d5e0828a
KimHaejoong1 <hjkim2124@gmail.com>
2025-06-08 16:43:37 +0900

    modified: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,6 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   async rewrites() {
-//     const apiBaseUrl = process.env.API_BASE_URL;
-//     console.log("API Base URL:", apiBaseUrl);
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination: `${apiBaseUrl}/api/:path*`,
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
