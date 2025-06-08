46543992a797c39c4c45a1490cff7adf3e74e04c
KimHaejoong1 <hjkim2124@gmail.com>
2025-06-08 16:31:49 +0900

    fix: eslint error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   async rewrites() {
+//     const apiBaseUrl = process.env.API_BASE_URL;
+//     console.log("API Base URL:", apiBaseUrl);
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: `${apiBaseUrl}/api/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
